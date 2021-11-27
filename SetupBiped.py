7bdaf1105210da28212d8d4dcabebf037c407364
Laura Harman <lrharman@student.unimelb.edu.au>
2021-11-27 16:19:37 +1100

    saved

--- SetupBiped.py
+++ SetupBiped.py
@@ -8,7 +8,11 @@ class SetupBiped:
         self.sister = None
         self.a = a
         self.q = q
-
+        self.pos=vector(0,0,0)
+    def  draw(self,pos):
+        joint_rad = 4
+        sphere(pos=self.pos, radius=joint_rad)
+        
 # setting a, joint axis vector (roll, pitch, yaw)
 UX = vector(1,0,0)
 UY = vector(0,1,0)
@@ -30,4 +34,17 @@ j11 = SetupBiped(8, UX, 0)
 j12 = SetupBiped(8, UX, 0)
 j13 = SetupBiped(8, UZ, 0)
 
-j1.child = j2
+j1.child = [j2, j8] 
+
+j2.child = j3
+#j2.sister = j8
+
+j3.child = j4
+j4.child = j5
+j5.child = j6
+j6.child = j7
+
+j8.child = j9
+j9.child = j10
+j10.child = j11
+j12.child = j13
